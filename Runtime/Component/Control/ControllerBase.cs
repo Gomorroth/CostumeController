f7495977ae3820294699211357c0076d1d5a193f
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2024-01-11 09:57:50 +0900

    fix: move `IControlTargetProvider` to `ControllerBase`

--- Runtime/Component/Control/ControllerBase.cs
+++ Runtime/Component/Control/ControllerBase.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 
 namespace gomoru.su.CostumeController.Components.Controls
 {
-    public abstract class ControllerBase : CCBaseComponent, IControl
+    public abstract class ControllerBase : CCBaseComponent, IControl, IControlTargetProvider
     {
         public bool IsActiveByDefault;
 
         [SerializeReference]
         public List<OptionalControl> OptionalControls;
+
+        public void GetControlTargets(ref ValueList<GameObject> destination)
+        {
+            destination.Add(gameObject);
+        }
     }
 }
--- Runtime/Component/Control/OptionalControllerBase.cs
+++ Runtime/Component/Control/OptionalControllerBase.cs
@@ -3,13 +3,8 @@ using UnityEngine;
 
 namespace gomoru.su.CostumeController.Components.Controls
 {
-    public abstract class OptionalControllerBase<TControl> : ControllerBase, IControlTargetProvider where TControl : OptionalControl
+    public abstract class OptionalControllerBase<TControl> : ControllerBase where TControl : OptionalControl
     {
         public TControl Control;
-
-        public void GetControlTargets(ref ValueList<GameObject> destination)
-        {
-            destination.Add(gameObject);
-        }
     }
 }
