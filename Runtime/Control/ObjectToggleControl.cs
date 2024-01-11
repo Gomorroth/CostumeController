3b65b910df7a5f76ac44f172718d966fd9f59045
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2024-01-11 10:08:05 +0900

    feat: add `ObjectToggleControl`

---
+++ Runtime/Control/ObjectToggleControl.cs
@@ -0,0 +1,15 @@
+﻿using gomoru.su.CostumeController.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace gomoru.su.CostumeController
+{
+    [Serializable]
+    public sealed class ObjectToggleControl<T> : ControlBase<bool>
+    {
+    }
+}
