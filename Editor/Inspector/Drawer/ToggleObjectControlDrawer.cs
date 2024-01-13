c954e28bd77bf5e9483794c5ce2df1a1816eb3a6
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-01-13 15:02:02 +0900

    feat: add `ToggleObjectControlDrawer`

---
+++ Editor/Inspector/Drawer/ToggleObjectControlDrawer.cs
@@ -0,0 +1,30 @@
+﻿using gomoru.su.CostumeController.Controls;
+using UnityEditor;
+using UnityEngine;
+
+namespace gomoru.su.CostumeController
+{
+    [CustomPropertyDrawer(typeof(ToggleObjectControl))]
+    internal sealed class ToggleObjectControlDrawer : OptionalControlDrawer<ToggleObjectControl, ToggleObjectControlDrawer>
+    {
+        public override float GetPropertyCount(SerializedProperty property, GUIContent label) => 3;
+
+        public override void Draw(Rect position, SerializedProperty property, bool showTargetObject = true)
+        {
+            var targetObjectProp = property.FindPropertyRelative(nameof(OptionalControl.TargetObject));
+            var rootObj = (property.serializedObject.targetObject as Component)?.gameObject;
+            position.height = EditorGUIUtility.singleLineHeight;
+
+            if (showTargetObject)
+            {
+                DrawTargetObject(position, targetObjectProp, rootObj, typeof(GameObject));
+                NewLine(ref position);
+            }
+
+            EditorGUI.PropertyField(position, property.FindPropertyRelative(nameof(ControlBase<object>.Disabled)));
+            NewLine(ref position);
+
+            EditorGUI.PropertyField(position, property.FindPropertyRelative(nameof(ControlBase<object>.Enabled)));
+        }
+    }
+}
