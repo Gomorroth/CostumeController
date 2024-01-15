ad32720f7a1258c6e3fa5136aba4230facf5059b
Grabacr Scarlet <73260157+GrabacrScarlet@users.noreply.github.com>
2024-01-15 10:10:47 +0900

    feat: add `IHierarchyChangedCallback`

---
+++ Editor/EventHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace gomoru.su.CostumeController
+{
+    [InitializeOnLoad]
+    internal static class EventHandler
+    {
+        static EventHandler()
+        {
+            EditorApplication.hierarchyChanged += OnHierarchyChanged;
+        }
+
+        private static void OnHierarchyChanged()
+        {
+            foreach (var obj in Object.FindObjectsByType(typeof(Component), FindObjectsInactive.Include, FindObjectsSortMode.None))
+            {
+                if (obj is IHierarchyChangedCallback callback)
+                {
+                    callback.OnHierarchyChanged();
+                }
+            }
+        }
+    }
+}
